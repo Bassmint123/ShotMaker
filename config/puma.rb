2b01fdf8203c63e4c83965ea6335d3b9e766787e
Bassmint123 <bradb@cimstrat.com>
2017-04-04 20:40:28 -0500

    make it better

--- config/puma.rb
+++ config/puma.rb
@@ -43,7 +43,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 # end
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 4)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
