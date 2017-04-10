9b9ec0783695f7fca99bad2f471bb5e2ab97c03e
Bassmint123 <bradb@cimstrat.com>
2017-04-09 23:24:18 -0500

    added login with changing links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user    # automatically login user after sign-up.
       flash[:success] = "Welcome to ShotMaker!"
       redirect_to @user
     else
