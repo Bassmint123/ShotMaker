22a94cf05972436f3ec72d1529d87138e3247a8e
Bassmint123 <bradb@cimstrat.com>
2017-04-16 13:16:51 -0500

    Added Edit page for profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }  #password length needs to be at least six characters
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true  #password length needs to be at least six characters
 
   # Returns the hash digest of the given string.
   def User.digest(string)
