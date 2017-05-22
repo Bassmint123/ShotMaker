913d10d2645f73ee57ac9762031f99a0190174d6
Bassmint123 <bradb@cimstrat.com>
2017-05-21 22:38:11 -0500

    fixed admin delete function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]  # Make sure the user is logged in first
-  before_action :correct_user, only: [:edit, :update]  # To redirect users trying to edit another user’s profile
-  before_action :admin_user, only: :destroy
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]  # Make sure the user is logged in first
+  before_action :correct_user,   only: [:edit, :update]  # To redirect users trying to edit another user’s profile
+  before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -71,8 +71,8 @@ class UsersController < ApplicationController
     end
 
   # Confirms an admin user.
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
