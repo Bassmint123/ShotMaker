1a8fae3850fc740c33aa03f3e7caa110cce1dbe6
Bassmint123 <bradb@cimstrat.com>
2017-08-31 10:03:16 -0500

    fixed password_reset format page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824153101) do
+ActiveRecord::Schema.define(version: 20170831040029) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "ingredientname"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20170824153101) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
